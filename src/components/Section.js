763f5ebf91ca3e03e91f27d0e79cb59821597faa
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-06-12 11:52:10 +0530

    add reveal effects

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Section.css';
+import Zoom from 'react-reveal/Zoom';
+import Flip from 'react-reveal/Flip';
+
+
 
 export default function Section(props) {
     const newClassName = "section " + (props.className ? props.className : "");
     return (
         <section className={newClassName}>
             <div className="container">
-                <div className="content">
-                    <h1 className="title">
-                        {props.title}
-                    </h1>
-                    
-                    <p className="summary">
-                        {props.summary}
-                    </p>
+                <Zoom>
+                    <div className="content">
+                        <Flip>
+                            <h1 className="title">
+                                {props.title}
+                            </h1>
+                        </Flip>
+                        
+                        <p className="summary">
+                            {props.summary}
+                        </p>
 
-                    {props.children}
-                </div>
+                        {props.children}
+                    </div>
+                </Zoom>
             </div>
 
         </section>
